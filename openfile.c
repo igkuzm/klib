bb30b966ac207fe0bb9e1ad11c5b61c8ab28e5da
Igor Sementsov <ig.kuzm@gmail.com>
2022-03-18 23:16:02 +1000

    ios_openfile_fix

--- openfile.c
+++ openfile.c
@@ -2,7 +2,7 @@
  * File              : openfile.c
  * Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
  * Date              : 18.09.2021
- * Last Modified Date: 22.02.2022
+ * Last Modified Date: 18.03.2022
  * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
  */
 #include "openfile.h"
@@ -15,8 +15,11 @@
 int k_lib_openfile(const char *filename){
 	char open_file_command[BUFSIZ];
 #ifdef __APPLE__
-	sprintf(open_file_command, "open %s", filename);	
-	system(open_file_command);
+	#if IOS
+	#else
+		sprintf(open_file_command, "open %s", filename);	
+		system(open_file_command);
+	#endif
 #elif defined _WIN32 || defined _WIN64
 	ShellExecute(NULL, "open", filename, NULL, NULL, SW_SHOWDEFAULT);
 #else
