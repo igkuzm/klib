b4e489d42172a3f83eeb41dcd2b63e984a3ef754
Igor Sementsov <ig.kuzm@gmail.com>
2022-05-26 18:18:20 +1000

    update

--- cp.h
+++ cp.h
@@ -2,7 +2,7 @@
  * File              : cp.h
  * Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
  * Date              : 04.09.2021
- * Last Modified Date: 07.03.2022
+ * Last Modified Date: 26.05.2022
  * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
  */
 #ifndef k_lib_cp_h__
@@ -13,6 +13,9 @@ extern "C"{
 #endif
 
 #include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 //copy file FROM path to destination TO path
 int k_lib_cp(const char *from, const char *to);
@@ -24,6 +27,66 @@ int k_lib_cp(const char *from, const char *to);
 	___c;	\
 })
 #endif
+
+int k_lib_cp(const char *from, const char *to)
+{
+    int fd_to, fd_from;
+    char buf[4096];
+    ssize_t nread;
+    int saved_errno;
+
+    fd_from = open(from, O_RDONLY);
+    if (fd_from < 0)
+        return -1;
+
+	fd_to = open(to, O_WRONLY | O_CREAT | O_EXCL, 0666);
+    
+	if (fd_to < 0)
+        goto out_error;
+
+    while (nread = read(fd_from, buf, sizeof buf), nread > 0)
+    {
+        char *out_ptr = buf;
+        ssize_t nwritten;
+
+        do {
+            nwritten = write(fd_to, out_ptr, nread);
+
+            if (nwritten >= 0)
+            {
+                nread -= nwritten;
+                out_ptr += nwritten;
+            }
+            else if (errno != EINTR)
+            {
+                goto out_error;
+            }
+        } while (nread > 0);
+    }
+
+    if (nread == 0)
+    {
+        if (close(fd_to) < 0)
+        {
+            fd_to = -1;
+            goto out_error;
+        }
+        close(fd_from);
+
+        /* Success! */
+        return 0;
+    }
+
+  out_error:
+    saved_errno = errno;
+
+    close(fd_from);
+    if (fd_to >= 0)
+        close(fd_to);
+
+    errno = saved_errno;
+    return -1;
+}
 
 #ifdef __cplusplus
 }
--- openfile.h
+++ openfile.h
@@ -2,7 +2,7 @@
  * File              : openfile.h
  * Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
  * Date              : 18.09.2021
- * Last Modified Date: 16.03.2022
+ * Last Modified Date: 26.05.2022
  * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
  */
 
@@ -14,6 +14,11 @@ extern "C"{
 #endif
 
 #include <stdio.h>
+#include <strings.h>
+#include <stdlib.h>
+#if defined _WIN32 || defined _WIN64
+#include <windows.h>
+#endif	
 
 int k_lib_openfile(const char *filename);
 
@@ -24,6 +29,28 @@ int k_lib_openfile(const char *filename);
 	___c;	\
 })
 #endif
+
+int k_lib_openfile(const char *filename){
+	char open_file_command[BUFSIZ];
+#ifdef __APPLE__
+	#include <TargetConditionals.h>
+	#if TARGET_IPHONE_SIMULATOR
+	#elif TARGET_OS_MACCATALYST
+	#elif TARGET_OS_IPHONE
+	#elif TARGET_OS_MAC
+		sprintf(open_file_command, "open %s", filename);	
+		system(open_file_command);
+	#else
+	#endif
+#elif defined _WIN32 || defined _WIN64
+	ShellExecute(NULL, "open", filename, NULL, NULL, SW_SHOWDEFAULT);
+#else
+	sprintf(open_file_command, "xdg-open %s", filename);	
+	system(open_file_command);
+#endif	
+	
+	return 0;
+}
 
 #ifdef __cplusplus
 }
