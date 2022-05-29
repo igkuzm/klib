3bbdcc19ef974514b751334ba92f30d30f633c28
Igor Sementsov <ig.kuzm@gmail.com>
2022-05-29 19:26:06 +1000

    new cp

--- cp.h
+++ cp.h
@@ -2,7 +2,7 @@
  * File              : cp.h
  * Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
  * Date              : 04.09.2021
- * Last Modified Date: 26.05.2022
+ * Last Modified Date: 29.05.2022
  * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
  */
 #ifndef k_lib_cp_h__
@@ -30,62 +30,24 @@ int k_lib_cp(const char *from, const char *to);
 
 int k_lib_cp(const char *from, const char *to)
 {
-    int fd_to, fd_from;
-    char buf[4096];
-    ssize_t nread;
-    int saved_errno;
+	FILE * fp_from = fopen(from, "r");
+	if (!fp_from) 
+		return -1;
 
-    fd_from = open(from, O_RDONLY);
-    if (fd_from < 0)
-        return -1;
+	FILE * fp_to = fopen(to, "w");
+	if (!fp_to){
+		fclose(fp_from);
+		return -1;
+	} 
 
-	fd_to = open(to, O_WRONLY | O_CREAT | O_EXCL, 0666);
-    
-	if (fd_to < 0)
-        goto out_error;
+	int ch;
+	while ((ch = fgetc(fp_from)) != EOF)
+		fputc(ch, fp_to);
+	
+	fclose(fp_from);
+	fclose(fp_to);
 
-    while (nread = read(fd_from, buf, sizeof buf), nread > 0)
-    {
-        char *out_ptr = buf;
-        ssize_t nwritten;
-
-        do {
-            nwritten = write(fd_to, out_ptr, nread);
-
-            if (nwritten >= 0)
-            {
-                nread -= nwritten;
-                out_ptr += nwritten;
-            }
-            else if (errno != EINTR)
-            {
-                goto out_error;
-            }
-        } while (nread > 0);
-    }
-
-    if (nread == 0)
-    {
-        if (close(fd_to) < 0)
-        {
-            fd_to = -1;
-            goto out_error;
-        }
-        close(fd_from);
-
-        /* Success! */
-        return 0;
-    }
-
-  out_error:
-    saved_errno = errno;
-
-    close(fd_from);
-    if (fd_to >= 0)
-        close(fd_to);
-
-    errno = saved_errno;
-    return -1;
+	return 0;
 }
 
 #ifdef __cplusplus
