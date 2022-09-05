9b8b3152aec97615bf2d4689470ff93c82c3e1bc
Igor V. Sementsov <ig.kuzm@gmail.com>
2022-09-06 09:32:48 +1000

    Updated isdir.h

--- isdir.h
+++ isdir.h
@@ -2,8 +2,12 @@
  * File              : isdir.h
  * Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
  * Date              : 29.05.2022
- * Last Modified Date: 14.08.2022
+ * Last Modified Date: 06.09.2022
  * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
+ */
+
+/*
+ * checks if is directory at path
  */
 
 #ifndef k_lib_is_directory_h_
