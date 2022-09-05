5c52512d8b79728af5194ab9b5a1f56eb3bdc4a4
Igor V. Sementsov <ig.kuzm@gmail.com>
2022-09-06 09:13:33 +1000

    Updated array.h

--- array.h
+++ array.h
@@ -2,8 +2,17 @@
  * File              : array.h
  * Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
  * Date              : 22.02.2022
- * Last Modified Date: 22.07.2022
+ * Last Modified Date: 06.09.2022
  * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
+ */
+
+/*
+ * C library for dynamic arrays
+ * struct array_t * array_new(T) - allocate and return new array of types T
+ * void array_append(struct array_t * array, T, T item) - add item to array with type T
+ * T item * array_at(struct array_t * array, T, int i) - return item from array at position i
+ * array_for_each(struct array_t * array, T, T item){} - iterate the array
+ * void array_free(struct array_t * array) - free array
  */
 
 #ifndef k_lib_array_h__
@@ -21,14 +30,14 @@ struct array_t {
 	int len;
 };
 
-#define array_new(type)\
+#define array_new(T)\
 	({\
 		struct array_t * ___array = malloc(sizeof(struct array_t));\
 		if (___array == NULL) {\
 			perror("array_new malloc");\
 			exit(EXIT_FAILURE);\
 		}\
-		___array->data = malloc(sizeof(type));\
+		___array->data = malloc(sizeof(T));\
 		if (___array->data == NULL) {\
 			perror("array_new data malloc");\
 			exit(EXIT_FAILURE);\
@@ -37,27 +46,27 @@ struct array_t {
 		___array;\
 	 })
 
-#define array_append(array, type, item) \
+#define array_append(array, T, item) \
 	({ \
-		type * ___data = array->data;\
+		T * ___data = array->data;\
 		___data[array->len] = item;\
 		array->len++;\
-		array->data = realloc(array->data, sizeof(type) + sizeof(type) * array->len);\
+		array->data = realloc(array->data, sizeof(T) + sizeof(T) * array->len);\
 		if (array->data == NULL) {\
 			perror("array_append_item realloc");\
 			exit(EXIT_FAILURE);\
 		}\
 	})
 
-#define array_at(array, type, i)\
+#define array_at(array, T, i)\
 	({\
-		type * ___data = array->data;\
+		T * ___data = array->data;\
 		___data[i];\
 	})
 
-#define array_for_each(array, type, item)\
-		type * ___data = array->data;\
-		type * ___p, item;\
+#define array_for_each(array, T, item)\
+		T * ___data = array->data;\
+		T * ___p, item;\
 		for (___p = (___data), (item) = *___p; ___p < &((___data)[array->len]); ___p++, (item) = *___p)\
 
 #define array_free(array)\
