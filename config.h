9892cad2ce5b1353cb0207b49cdf9b239c786a87
Igor V. Sementsov <ig.kuzm@gmail.com>
2023-02-02 21:11:13 +1000

    fix

--- config.h
+++ config.h
@@ -1,3 +1,10 @@
+/**
+ * File              : config.h
+ * Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
+ * Date              : 02.02.2023
+ * Last Modified Date: 02.02.2023
+ * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
+ */
 /* Read config file */
 
 #ifndef k_lib_config_h__
@@ -11,38 +18,54 @@ extern "C" {
 
 #define CONFIG_ARG_MAX_BYTES 128
 
-void read_config_file(char* path, void *user_data, int (*callback)(void *user_data, char *key, char *value)) {
-    FILE* fp;
-    
-    if ((fp = fopen(path, "r+")) == NULL) {
-        perror("fopen()");
-        return NULL;
-    }
-    
-    while(1) {
-        char key[CONFIG_ARG_MAX_BYTES];
-        char value[CONFIG_ARG_MAX_BYTES];
-        
-        if (fscanf(fp, "%s = %s", key, value) != 2) {
-            //perror("fscanf()");
-            continue;
-        }
-        
-        if (key[0] == '#') {
-            //while (fgetc(fp) != '\n') {
-            // Do nothing (to move the cursor to the end of the line).
-            //}
-           continue;
-        }
+/*
+ * read file and run callback for each key/value pair
+ */
+void read_config_file(const char* path, void *user_data, 
+		int (*callback)(void *user_data, char *key, char *value)) 
+{
+  FILE* fp;
+	char buf[BUFSIZ], key[CONFIG_ARG_MAX_BYTES], value[CONFIG_ARG_MAX_BYTES];
+	int i, ch, blen=0, klen=0, vlen=0, first=0, match=0, comment=0;
 
-        //printf("Key: %s\nValue: %s\n", key, value);
-        if (callback)
-            if (!callback(user_data, key, value)) //stop exec if callback != 0
-                break;
-        
-        if (feof(fp))
-            break;
-    }
+  if ((fp = fopen(path, "r+")) == NULL) {
+      perror("fopen()");
+      return;
+  }
+
+	while((ch=fgetc(fp))!=EOF){
+		if (ch=='#' && first)
+			comment = 1;	
+		if (ch=='=') {
+			key[klen+1] = 0;
+			match = 1;
+		}
+		if (ch=='\n'){
+			if (match & !comment) {
+				value[vlen+1] = 0;
+				if (callback)
+					if (callback(user_data, key, value))
+						break;
+			}
+			for (i = 0; i < klen; ++i)
+				key[i] = 0;
+			klen = 0;
+			for (i = 0; i < vlen; ++i)
+				value[i] = 0;
+			vlen = 0;
+			match = 0;
+			comment = 0;
+			first = 1;
+			continue;
+		}
+		if (match)
+			value[vlen++] = ch;
+		else
+			key[klen++] = ch;
+		first = 0;
+	}
+
+	fclose(fp);
 }
 
 #ifdef __cplusplus
