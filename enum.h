1baec54464c86baacd36982a2f8181e0f96e872a
Igor V. Sementsov <ig.kuzm@gmail.com>
2023-04-24 15:35:49 +1000

    fix

--- enum.h
+++ enum.h
@@ -8,27 +8,50 @@
 
 /*
  * macros for emun
+ *
+ * to use include:
+ *
+ * #define DAYS \
+ *	DAY(monday)\
+ *	DAY(tuesday)\
+ *	DAY(wednesday)\
+ *	...etc
+ *
+ * BEGIN_ENUM(Days){
+ *		#define DAY(x) DECL_ENUM_ELEMENT(x), 
+ *      DAYS
+ *      #undef DAY
+ * }
+ * END_ENUM(Days)
+ *
+ * BEGIN_ENUM_STRING( Days ){
+ *		#define DAY(x) DECL_ENUM_STRING_ELEMENT(x), 
+ *      DAYS
+ *      #undef DAY
+ * }
+ * END_ENUM_STRING(Days)
+ * 
+ * then you can use: 
+ *
+ * switch(day)
+ *	case monday: break;
+ *	case tuesday: break;
+ *	case wednesday: 
+ *			printf("Today is %s\n", getStringDays(day));
+ *			break;
+ *
  */
-#ifdef DECL_ENUM_ELEMENT
-#undef DECL_ENUM_ELEMENT 
-#endif
 
-#ifdef BEGIN_ENUM
-#undef BEGIN_ENUM 
-#endif
+#ifndef ENUM_H
+#define ENUM_H
 
-#ifdef END_ENUM
-#undef END_ENUM 
-#endif
-
-#ifndef GENERATE_ENUM_STRINGS
-    #define DECL_ENUM_ELEMENT( element ) element
     #define BEGIN_ENUM( ENUM_NAME ) typedef enum tag##ENUM_NAME
-    #define END_ENUM( ENUM_NAME ) ENUM_NAME; \
-            char* getString##ENUM_NAME(enum tag##ENUM_NAME index);
-#else
-    #define DECL_ENUM_ELEMENT( element ) #element
-    #define BEGIN_ENUM( ENUM_NAME ) char* gs_##ENUM_NAME [] =
-    #define END_ENUM( ENUM_NAME ) ; char* getString##ENUM_NAME(enum \
+    #define DECL_ENUM_ELEMENT( element ) element
+    #define END_ENUM( ENUM_NAME ) ENUM_NAME;
+    
+    #define BEGIN_ENUM_STRING( ENUM_NAME ) static char* gs_##ENUM_NAME [] =
+	#define DECL_ENUM_STRING_ELEMENT( element ) #element
+    #define END_ENUM_STRING( ENUM_NAME ) ; static char* getString##ENUM_NAME(enum \
             tag##ENUM_NAME index){ return gs_##ENUM_NAME [index]; }
-#endif
+
+#endif /* ifndef ENUM_H */
