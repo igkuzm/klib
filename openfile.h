84577cc69a11a48f54512b1466754d2e07a0d880
Igor V. Sementsov <ig.kuzm@gmail.com>
2023-06-02 11:14:48 +1000

    fix error with mingw compile

--- openfile.h
+++ openfile.h
@@ -2,7 +2,7 @@
  * File              : openfile.h
  * Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
  * Date              : 18.09.2021
- * Last Modified Date: 21.01.2023
+ * Last Modified Date: 02.06.2023
  * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
  */
 
@@ -20,7 +20,11 @@ extern "C" {
 #include <stdio.h>
 
 int openfile(const char *path) {
-#ifdef __APPLE__
+#if defined _WIN32_
+#include <windows.h>
+  ShellExecute(NULL, "open", path, NULL, NULL, SW_SHOWDEFAULT);
+
+#elif defined __APPLE__
 #include <objc/objc-runtime.h>
   id str = ((id (*)(Class, SEL, const char *))objc_msgSend)(
       objc_getClass("NSString"), sel_registerName("stringWithUTF8String:"),
@@ -28,22 +32,19 @@ int openfile(const char *path) {
   id url = ((id (*)(Class, SEL, id))objc_msgSend)(
       objc_getClass("NSURL"), sel_registerName("fileURLWithPath:"), str);
 #include <TargetConditionals.h>
-#if TARGET_OS_MAC
-  id ws = ((id (*)(Class, SEL))objc_msgSend)(
-      objc_getClass("NSWorkspace"), sel_registerName("sharedWorkspace"));
-  ((void (*)(id, SEL, id))objc_msgSend)(ws, sel_registerName("openURL:"), url);
-#else
-  id app = ((id (*)(Class, SEL))objc_msgSend)(
-      objc_getClass("UIApplication"), sel_registerName("sharedApplication"));
-  id opt = ((id (*)(Class, SEL))objc_msgSend)(objc_getClass("NSDictionary"),
+	#if TARGET_OS_MAC
+		id ws = ((id (*)(Class, SEL))objc_msgSend)(
+			objc_getClass("NSWorkspace"), sel_registerName("sharedWorkspace"));
+		((void (*)(id, SEL, id))objc_msgSend)(ws, sel_registerName("openURL:"), url);
+	#else
+		id app = ((id (*)(Class, SEL))objc_msgSend)(
+			objc_getClass("UIApplication"), sel_registerName("sharedApplication"));
+		id opt = ((id (*)(Class, SEL))objc_msgSend)(objc_getClass("NSDictionary"),
                                               sel_registerName("dictionary"));
-  ((void (*)(id, SEL, id, id, id))objc_msgSend)(
-      app, sel_registerName("openURL:options:completionHandler:"), url, opt,
-      NULL);
-#endif
-#elif defined _WIN32
-#include <windows.h>
-  ShellExecute(NULL, "open", path, NULL, NULL, SW_SHOWDEFAULT);
+			((void (*)(id, SEL, id, id, id))objc_msgSend)(
+			app, sel_registerName("openURL:options:completionHandler:"), url, opt,
+			NULL);
+	#endif
 #else
 #include <stdlib.h>
 #include <strings.h>
