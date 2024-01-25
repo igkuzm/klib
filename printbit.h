1a74a3cb4a33b3c1a82aaaf4683a85714003f76a
Igor V. Sementsov <ig.kuzm@gmail.com>
2024-01-25 10:55:24 +1000

    print64_bit

--- printbit.h
+++ printbit.h
@@ -2,7 +2,7 @@
  * File              : printbit.h
  * Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
  * Date              : 22.02.2022
- * Last Modified Date: 21.01.2023
+ * Last Modified Date: 25.01.2024
  * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
  */
 
@@ -16,12 +16,23 @@ extern "C"{
 #include <stdint.h>
 #include <stdio.h>
 
+static void print64bit(int64_t c);
 static void print32bit(int32_t c);
 static void print8bit (int8_t c);
 
 /*
  * IMP
  */
+
+void 
+print64bit(int64_t c) {
+	for (int i = 63; i >= 0; --i) {
+		if (i == 7 || i == 15 || i == 23 || i == 31 || i == 39 || i == 47 || i == 55)
+			printf(" ");
+	   printf("%c", (c & (1 << i)) ? '1' : '0');
+	}
+	printf("\n");
+}
 
 void 
 print32bit(int32_t c) {
