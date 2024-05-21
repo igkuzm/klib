d1deaa66d672a5f5301367fd26c3c2e20eb3d8a3
Igor V. Sementsov <kuzmich@macbook.kuzm>
2024-05-21 15:01:47 +1000

    log fix

--- log.h
+++ log.h
@@ -2,7 +2,7 @@
  * File              : log.h
  * Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
  * Date              : 19.03.2023
- * Last Modified Date: 19.03.2023
+ * Last Modified Date: 21.05.2024
  * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
  */
 
@@ -20,20 +20,20 @@ extern "C" {
 #include <stdio.h>
 #include <string.h>
 
-#define STR(...) ({char s[BUFSIZ]; sprintf(s, __VA_ARGS__); s;})
+#define STR(...) ({char s[BUFSIZ]; snprintf(s, BUFSIZ-1, __VA_ARGS__); s[BUFSIZ-1]=0; s;})
 
 #ifdef __ANDROID__
 	#include <android/log.h>
 	#define LOG(...) __android_log_print(ANDROID_LOG_INFO,  STR("%s: _%s: ", __FILE__, __func__), __VA_ARGS__)
-	#define ERR(...) __android_log_print(ANDROID_LOG_ERROR, STR("%s: _%s: ", __FILE__, __func__), __VA_ARGS__) 	
+	#define ERR(...) __android_log_print(ANDROID_LOG_ERROR, STR("%s: _%s: %d: ", __FILE__, __func__, __LINE__), __VA_ARGS__) 	
 #elif defined __APPLE__
 	#include <CoreFoundation/CoreFoundation.h>
 	void NSLog(CFStringRef format, ...);
 	#define LOG(fmt, ...) NSLog(CFSTR(fmt), ##__VA_ARGS__)
-	#define ERR(fmt, ...) LOG("E/_%s: %s", __func__, STR(fmt, __VA_ARGS__)) 
+	#define ERR(fmt, ...) LOG("E/_%s: %d: %s", __func__, __LINE__, STR(fmt, __VA_ARGS__)) 
 #else
-	#define LOG(...) printf("%s: _%s: %s\n",   __FILE__, __func__, STR(__VA_ARGS__))
-	#define ERR(...) perror(STR("%s: _%s: %s", __FILE__, __func__, STR(__VA_ARGS__))) 
+	#define LOG(...) fprintf(stderr, "%s: _%s: %s\n",   __FILE__, __func__, STR(__VA_ARGS__))
+	#define ERR(...) perror(STR("%s: _%s: %d: %s", __FILE__, __func__, __LINE__, STR(__VA_ARGS__))) 
 #endif
 
 
