3c867b295ff911149b1c7752bfa5e8a6a1622fb5
Igor V. Sementsov <kuzmich@macbook.kuzm>
2024-09-21 22:38:17 +1000

    curl_transfer

---
+++ curl_transfer.h
@@ -0,0 +1,275 @@
+/**
+ * File              : curl_transfer.h
+ * Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
+ * Date              : 21.09.2024
+ * Last Modified Date: 21.09.2024
+ * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
+ */
+/**
+ * curl_transfer.h
+		
+ * Copyright (c) 2024 Igor V. Sementsov <ig.kuzm@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+		
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+		
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+		
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+		
+/* CURL functions to download/upload files */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <curl/curl.h>
+
+#define CURL_TRANSFER_VERIFY_SSL 1L
+
+/* function to handle download/upload progress */
+typedef int 
+curl_transfer_progress(void *ptr, 
+		double dltotal, double dlnow, 
+		double ultotal, double ulnow); 
+
+/* download url to FILE stream
+ * %fp - pointer to FILE stream
+ * %url - null-terminated string with url address
+ * %error - pointer to allocated error string or NULL;
+ * %ptr - pointer to data for progress function or NULL
+ * %progress - pointer to progress function or NULL */
+static size_t curl_transfer_download_file(
+		FILE *fp, const char *url, char **error, 
+		void *ptr, curl_transfer_progress *progress);
+
+/* download url to memory
+ * %data - pointer to allocated memory with downloaded data
+ * %url - null-terminated string with url address
+ * %error - pointer to allocated error string or NULL;
+ * %ptr - pointer to data for progress function or NULL
+ * %progress - pointer to progress function or NULL */
+static size_t curl_transfer_download_data(
+		void **data, const char *url, char **error, 
+		void *ptr, curl_transfer_progress *progress);
+
+/* upload FILE stream to url
+ * %fp - pointer to FILE stream
+ * %url - null-terminated string with url address
+ * %error - pointer to allocated error string or NULL;
+ * %ptr - pointer to data for progress function or NULL
+ * %progress - pointer to progress function or NULL */
+static size_t curl_transfer_upload_file(
+		FILE *fp, const char *url, char **error, 
+		void *ptr, curl_transfer_progress *progress);
+
+/* upload data to memory
+ * %data - data to upload
+ * %size - data size
+ * %url - null-terminated string with url address
+ * %error - pointer to allocated error string or NULL;
+ * %ptr - pointer to data for progress function or NULL
+ * %progress - pointer to progress function or NULL */
+static size_t curl_transfer_upload_data(
+		void *data, size_t size, const char *url, char **error, 
+		void *ptr, curl_transfer_progress *progress);
+
+/*
+ * IMPLIMATION
+ */
+
+struct curl_transfer_data {
+	unsigned char *data;
+	int size;
+};
+
+static size_t _curl_transfer_data_writefunc(
+		unsigned char  *data, size_t s, size_t n, 
+		struct curl_transfer_data *d)
+{
+	if (!d || !d->data)
+		return 0;
+	size_t size = s * n;
+	d->data = 
+		(unsigned char *)realloc(d->data, d->size + size);
+	if (!d->data)
+		return 0;
+
+	memcpy(&(d->data)[d->size], data, size);
+	d->size += size;
+	
+	return size;
+}
+
+static size_t _curl_transfer_upload_data_readfunc(
+		unsigned char *data, size_t s, size_t n, 
+		struct curl_transfer_data *d)
+{
+	size_t size = s * n;
+	
+	if (size > d->size)
+		size = d->size;
+
+	memcpy(data, d->data, size);
+	
+	d->data += size;
+	d->size -= size;
+
+	return s;
+}
+
+static size_t _curl_transfer_upload_file_readfunc(
+		char *data, size_t s, size_t n, 
+		FILE *fp)
+{
+	return fread(data, s, n, fp);
+}
+
+static CURL * _curl_transfer_init(
+		char fDownload,
+		FILE *fp, struct curl_transfer_data *data, size_t size, 
+		const char * url, char **error, 
+		void *ptr, 
+		curl_transfer_progress *progress)
+{
+	CURL *curl = curl_easy_init();
+	if (curl) {
+		curl_easy_setopt(curl, CURLOPT_URL, url);
+		if (fp && fDownload)
+			curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);		
+		if (data && fDownload) {
+			curl_easy_setopt(curl, CURLOPT_WRITEDATA, data);		
+			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, 
+					_curl_transfer_data_writefunc);
+		}
+		
+		/* enable verbose for easier tracing */
+		/*curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);*/
+
+		if (fDownload)
+			curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);		
+		else
+			curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
+		
+
+		if (fp && !fDownload){
+			curl_easy_setopt(curl, CURLOPT_READDATA, fp);
+			curl_easy_setopt(curl, CURLOPT_READFUNCTION, 
+					_curl_transfer_upload_file_readfunc);
+		}
+
+		if (data && !fDownload){
+			curl_easy_setopt(curl, CURLOPT_READDATA, data);
+			curl_easy_setopt(curl, CURLOPT_READFUNCTION, 
+					_curl_transfer_upload_data_readfunc);
+			curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE,
+				 	(curl_off_t)size);
+		}
+		
+		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 
+				CURL_TRANSFER_VERIFY_SSL);		
+		
+		if (progress) {
+			curl_easy_setopt(curl, CURLOPT_PROGRESSDATA, ptr);
+			curl_easy_setopt(curl, CURLOPT_PROGRESSFUNCTION, 
+					progress);
+			curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0);
+		}
+	}
+	return curl;
+}
+
+static size_t _curl_transfer_perfom(
+		char fDownload, CURL *curl, char **error)
+{
+	CURLcode res = curl_easy_perform(curl);
+	if (res != CURLE_OK) {
+		if (error)
+			*error = strdup(curl_easy_strerror(res));
+		curl_easy_cleanup(curl);
+		return 0;
+	}
+	/* now extract transfer info */
+	curl_off_t size;
+	if (fDownload)
+		curl_easy_getinfo(curl, 
+				CURLINFO_CONTENT_LENGTH_DOWNLOAD_T, &size);
+	else 
+		curl_easy_getinfo(curl, 
+				CURLINFO_CONTENT_LENGTH_UPLOAD_T, &size);
+	/* always cleanup */
+	curl_easy_cleanup(curl);
+	return size;
+}
+		
+size_t curl_transfer_download_file(
+		FILE *fp, const char *url, char **error, 
+		void *ptr, 
+		curl_transfer_progress *progress)
+{
+	CURL *curl = 
+		_curl_transfer_init(
+				1, fp, NULL, 0, 
+				url, error, ptr, progress);
+	if (curl)	
+		return _curl_transfer_perfom(1, curl, error);	
+	
+	return 0;
+}
+
+size_t curl_transfer_download_data(
+		void **data, const char *url, char **error, 
+		void *ptr, 
+		curl_transfer_progress *progress)
+{
+	struct curl_transfer_data d = {NULL, 0};
+	CURL *curl = 
+		_curl_transfer_init(
+				1, NULL, &d, 0, 
+				url, error, ptr, progress);
+	if (curl)	
+		return _curl_transfer_perfom(1, curl, error);	
+	
+	return 0;
+}
+
+size_t curl_transfer_upload_file(
+		FILE *fp, const char *url, char **error, 
+		void *ptr, 
+		curl_transfer_progress *progress)
+{
+	CURL *curl = 
+		_curl_transfer_init(
+				0, fp, NULL, 0, 
+				url, error, ptr, progress);
+	if (curl)	
+		return _curl_transfer_perfom(0, curl, error);	
+	
+	return 0;
+}
+
+size_t curl_transfer_upload_data(
+		void *data, size_t size, const char *url, char **error, 
+		void *ptr, 
+		curl_transfer_progress *progress)
+{
+	struct curl_transfer_data d = {NULL, 0};
+	CURL *curl = 
+		_curl_transfer_init(
+				0, NULL, &d, size, 
+				url, error, ptr, progress);
+	if (curl)	
+		return _curl_transfer_perfom(0, curl, error);	
+	
+	return 0;
+}
