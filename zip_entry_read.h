92d2734c75132dff3a61e4e806a95af6c31cd603
Igor V. Sementsov <kuzmich@macbook.kuzm>
2025-08-13 22:04:43 +1000

    fix

--- zip_entry_read.h
+++ zip_entry_read.h
@@ -1,9 +1,9 @@
 #ifndef ZIP_ENTRY_READ_H
 #define ZIP_ENTRY_READ_H
 
+#include <stdio.h>
 #include <zip.h>
 #include <stdlib.h>
-#include "log.h"
 #include "alloc.h"
 
 static int 
@@ -11,7 +11,7 @@ zip_entry_read(zip_t *zip, const char *name,
 		void **buffer, size_t *size)
 {
 #ifdef DEBUG
-	LOG("open entry: %s", name);
+	fprintf(stderr, "open entry: %s", name);
 #endif
 	zip_file_t *f = zip_fopen(zip, name, ZIP_FL_UNCHANGED);
 	if (!f)
@@ -20,12 +20,12 @@ zip_entry_read(zip_t *zip, const char *name,
 	struct zip_stat st;
 	zip_stat(zip, name, 0, &st);
 	void *buf = MALLOC(st.size, 
-			ERR("malloc"); 
+			perror("malloc");
 			zip_fclose(f);
 			return -1);
 		
 #ifdef DEBUG
-	LOG("reading %d bites to buffer", st.size);
+	fprintf(stderr, "reading %d bites to buffer", st.size);
 #endif
 	zip_fread(f, buf, st.size);
 
